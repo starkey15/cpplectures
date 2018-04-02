709572a0918b54f0a183e66d9c5f72c0c057bbbe
Colton Starkey <starkey15@live.missouristate.edu>
2018-04-02 13:10:39 -0500

    fixed code to be more dry, moved endl to a a statement outside of if-else statement so it is not repeated

--- main.cpp
+++ main.cpp
@@ -9,12 +9,14 @@ int main() {
 
     if (age > 20)
     {
-        std::cout << "Great! Buy the professor a drink!" << std::endl;
+        std::cout << "Great! Buy the professor a drink!";
     }
-
     else
     {
-        std::cout << "That's too bad." << std::endl;
+        std::cout << "That's too bad.";
     }
+
+    std::cout << std::endl;
+
     return 0;
 }
